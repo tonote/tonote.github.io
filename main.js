5b4ccee085956775aa0f6e6741173e81dad31428
Artur Gnedoy <archie682@hotmail.com>
2018-12-16 20:38:03 +0300

    Refactored UI

--- main.js
+++ main.js
@@ -1,14 +1,90 @@
-var CLIENT_ID = '778261473062-tr2ciii73p0pcgv5af7qre1cuhgmbhnm.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyCCshlpHHBSNtrrnUd0BqZs6FmjKJ9YM5k';
+const CLIENT_ID = '778261473062-tr2ciii73p0pcgv5af7qre1cuhgmbhnm.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyCCshlpHHBSNtrrnUd0BqZs6FmjKJ9YM5k';
 
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
-var SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+const SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly';
 
-var authorizeButton = document.getElementById('authorize_button');
-var signoutButton = document.getElementById('signout_button');
+const authorizeButton = document.getElementById('authorize_button');
+const signoutButton = document.getElementById('signout_button');
+
+loadedFile = null;
 
 function handleClientLoad() {
     gapi.load('client:auth2', initClient);
+}
+const contentContainer = document.getElementsByClassName('main')[0];
+
+notes.forEach(note => {
+    contentContainer.appendChild(createTextNote(note));
+});
+
+function createTextNote(note) {
+    const container = document.createElement('div');
+    container.classList.add('note-card', 'note-card-text', 'w3-card-4');
+
+    const header = document.createElement('header');
+    header.classList.add('margin-left-16');
+    const title = document.createElement('h4');
+    title.appendChild(document.createTextNode(note.title));
+    header.appendChild(title);
+    container.appendChild(header);
+
+    const content = document.createElement('div');
+    content.classList.add('w3-container');
+
+    if (note.checkings.length === 0) {
+        const contentText = document.createElement('p');
+        contentText.appendChild(document.createTextNode(note.content));
+        content.appendChild(contentText);
+    } else {
+        const checkings = note.checkings.split('');
+        const labels = note.content.split('\n');
+        for (let i = 0; i < checkings.length; i++) {
+            const item = document.createElement('div');
+            item.classList.add('check-item');
+            const input = document.createElement('input');
+            input.type = 'checkbox';
+            input.checked = checkings[i] === '1';
+            item.appendChild(input);
+
+            const label = document.createElement('label');
+            label.classList.add('margin-left-16');
+            label.appendChild(document.createTextNode(labels[i]));
+            item.appendChild(label);
+
+            content.appendChild(item);
+        }
+    }
+
+    container.appendChild(content);
+
+    const footer = document.createElement('footer');
+    footer.classList.add('margin-left-16');
+    const date = new Date(note.edited);
+    const dateString = document.createElement('p');
+    dateString.appendChild(document.createTextNode(`${date.toLocaleDateString()} ${date.toLocaleTimeString()}`));
+
+    footer.appendChild(dateString);
+    container.appendChild(footer);
+    container.style.backgroundColor = toColor(note.color.value);
+    return container;
+}
+
+function toColor(num) {
+    num >>>= 0;
+    const b = num & 0xFF,
+        g = (num & 0xFF00) >>> 8,
+        r = (num & 0xFF0000) >>> 16,
+        a = ( (num & 0xFF000000) >>> 24 ) / 255 ;
+    return `rgba(${r},${g},${b},${a})`;
+}
+
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignOutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
 }
 
 function initClient() {
@@ -19,158 +95,110 @@ function initClient() {
         scope: SCOPES
     }).then(function () {
         // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSignInStatus);
 
         // Handle the initial sign-in state.
-        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-        authorizeButton.onclick = handleAuthClick;
-        signoutButton.onclick = handleSignoutClick;
+        updateSignInStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
     }, function(error) {
         showMessage(JSON.stringify(error, null, 2));
     });
 }
 
-function updateSigninStatus(isSignedIn) {
-    var pre = document.getElementById('content');
+function showMessage(message) {
+    contentContainer.innerHTML = `<p class="message">` + message + `</p>`;
+}
 
+function updateSignInStatus(isSignedIn) {
     if (isSignedIn) {
-        authorizeButton.style.display = 'none';
         signoutButton.style.display = 'block';
 
-        pre.innerHTML = "";
+        contentContainer.innerHTML = "";
         listNotes();
     } else {
         authorizeButton.style.display = 'block';
         signoutButton.style.display = 'none';
 
-        pre.innerHTML = `<p class="message">To load notes from Google Drive, you need to <a onclick="gapi.auth2.getAuthInstance().signIn()" class="link">authorize</a>.</p>`;
+        const loginContainer = document.createElement('div');
+        loginContainer.classList.add('login-card', 'w3-card-4', 'w3-white');
+
+        const headerContainer = document.createElement('header');
+        const header = document.createElement('h3');
+        header.appendChild(document.createTextNode('Sign In'));
+        headerContainer.appendChild(header);
+
+        const loginContentContainer = document.createElement('div');
+        loginContentContainer.classList.add('w3-container');
+        const content = document.createElement('p');
+        content.appendChild(document.createTextNode('Use Google Account to see your notes'));
+        loginContentContainer.appendChild(content);
+
+        const footerContainer = document.createElement('footer');
+        const button = document.createElement('button');
+        button.classList.add('google-button');
+        button.onclick = handleAuthClick;
+
+        const iconSpan = document.createElement('span');
+        iconSpan.classList.add('google-button__icon');
+
+        const googleIcon = document.createElement('object');
+        googleIcon.type = 'image/svg+xml';
+        googleIcon.data = './assets/google-icon.svg';
+        iconSpan.appendChild(googleIcon);
+        button.appendChild(iconSpan);
+
+        const buttonTextSpan = document.createElement('span');
+        buttonTextSpan.classList.add('iconSpan');
+        const buttonText = document.createTextNode('Sign in with Google');
+        buttonTextSpan.appendChild(buttonText);
+        button.appendChild(buttonTextSpan);
+        footerContainer.appendChild(button);
+
+        loginContainer.appendChild(headerContainer);
+        loginContainer.appendChild(loginContentContainer);
+        loginContainer.appendChild(footerContainer);
+
+        contentContainer.appendChild(loginContainer);
     }
 }
 
-function handleAuthClick(event) {
-    gapi.auth2.getAuthInstance().signIn();
-}
-
-function handleSignoutClick(event) {
-    gapi.auth2.getAuthInstance().signOut();
-}
-
-function showMessage(message) {
-    var pre = document.getElementById('content');
-    pre.innerHTML = `<p class="message">` + message + `</p>`;
-}
-
-function toColor(num) {
-    num >>>= 0;
-    var b = num & 0xFF,
-        g = (num & 0xFF00) >>> 8,
-        r = (num & 0xFF0000) >>> 16,
-        a = ( (num & 0xFF000000) >>> 24 ) / 255 ;
-    return "rgba(" + [r, g, b, a].join(",") + ")";
-}
-
-function expandNote(noteId) {
-    var note = notes[noteId];
-    var card = document.getElementById(noteId);
-
-    var html = [`<div class="text">`];
-    
-    if (note.title.length > 0) {
-        html.push(`<div class="title">` + note.title + "</div>");
-    }
-    
-    var lines = note.content.split('\n');
-    lines.forEach(function(line) {
-        html.push("<p>" + line + "</p>");
-    })
-    
-    html.push("</div>");
-
-    var date = new Date(note.edited);
-    html.push(`<p class="footer">` + date.toLocaleDateString() + " " + date.getHours() + ":" + date.getMinutes() + `<text class="theme">` + note.color.theme + "</text></p>");
-
-    card.innerHTML = html.join("");
-    
-}
-
-function appendNote(note) {
-    var pre = document.getElementById('content');
-    var card = document.createElement('div');
-    card.className = "box";
-    card.style.backgroundColor = toColor(note.color.value);
-    card.id = note.id;
-
-    var html = [`<div class="text">`];
-    
-    if (note.title.length > 0) {
-        html.push(`<div class="title">` + note.title + "</div>");
-    }
-    
-    var lines = note.content.split('\n');
-    if(lines.length <= 10) {
-        lines.forEach(function(line) {
-            html.push("<p>" + line + "</p>");
-        })
-    } else {
-        for (var i = 0; i < 10; i++) {
-            html.push("<p>" + lines[i] + "</p>");
-        }
-
-        html.push(`<p style="color: black; font-size: 28px; cursor: pointer;" onclick="expandNote('${note.id}')">...</p>`);
-    }
-    
-    html.push("</div>");
-
-    var date = new Date(note.edited);
-    html.push(`<p class="footer">` + date.toLocaleDateString() + " " + date.getHours() + ":" + date.getMinutes() + `<text class="theme">` + note.color.theme + "</text></p>");
-
-    card.innerHTML = html.join("");
-    pre.appendChild(card);
-}
-
-loadedFile = null;
-
-function loadFile(file) {
-    gapi.client.drive.files.get({
-        'fileId': file.id,
-        'alt': 'media'
-    })
-    .then(function(response) {
-        loadedFile = JSON.parse(response.body);
-        notes = new Object();
-
-        loadedFile.notes.sort(function(a, b) {
-            return b.edited - a.edited;
-        });
-
-        loadedFile.notes.forEach(function(entry) {
-            if (!entry.hasOwnProperty('iv')) {
-                notes[entry.id] = entry;
-                appendNote(entry);
-            }
-        });
-
-    }, function(error) {
-        showMessage('Error during download. Try to reload.');
-        console.log("Error during download", error);
-    });
-}
-
-/**
- * Print notes.
- */
 function listNotes() {
     gapi.client.drive.files.list({
         'q': "name = 'notes_backup.json' and trashed = false",
         'pageSize': 10,
         'fields': "files(id, name)"
     }).then(function(response) {
-        var files = response.result.files;
+        const files = response.result.files;
         if(files.length == 0) {
             showMessage('There are no notes. Maybe you need to make backup from the android application.');
         } else {
-            var file = files[0];
+            const file = files[0];
             loadFile(file)
         }
     });
+}
+
+function loadFile(file) {
+    gapi.client.drive.files.get({
+        'fileId': file.id,
+        'alt': 'media'
+    })
+        .then(function(response) {
+            const loadedFile = JSON.parse(response.body);
+            const notes = new Object();
+
+            loadedFile.notes.sort(function(a, b) {
+                return b.edited - a.edited;
+            });
+
+            loadedFile.notes.forEach(function(entry) {
+                if (!entry.hasOwnProperty('iv')) {
+                    notes[entry.id] = entry;
+                    createTextNote(entry);
+                }
+            });
+
+        }, function(error) {
+            showMessage('Error during download. Try to reload.');
+            console.log("Error during download", error);
+        });
 }
